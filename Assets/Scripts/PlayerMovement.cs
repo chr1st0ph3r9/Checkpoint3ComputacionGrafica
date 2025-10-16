9be3c59d9821cf2921c37923c195722f3bb07610
christopher hayling <christopherhayling9@gmail.com>
2025-10-15 20:26:11 -0500

    update

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private PlayerInput inputJugador;
+    [SerializeField] private RigidBody rb;
+
+    [SerializeField] private float velocidad=2f;
+
+
+    private void OnEnable() {
+        
+
+    }
+    private void OnDisable() {
+        
+
+    }
+
+    private void Update() {
+        
+
+    }
+
+
+}
